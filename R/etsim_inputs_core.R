918ce4e92a65d56984155c32a2905ce6f3a72925
Ferran Espuny Pujol <Ferran.Espuny@gmail.com>
2023-09-18 19:03:57 +0100

    Fixing CRAN-check detected issues

--- R/etsim_inputs_core.R
+++ R/etsim_inputs_core.R
@@ -23,11 +23,11 @@
 #'
 #' @examples
 #'     #PENDING
-#'     #Generate input for a cubic b-spline hazard simulation with piecewise exponential survival censoring
+#'     #Generate input for a cubic b-spline hazard simulation
 #'     knots = c(0, 1, 3, 6, 10, NaN, NaN )
 #'     betac = 1 * c(0.05, 0.05, 0.05, 0.05, 0.40, 0.1, 0.05)
 #'     HParm = data.frame(knots, betac) # 'A Simple B-Spline'
-#'     INPUTS_hazard = etsim_inputs_core( HParam=HParm )
+#'     INPUTS_hazard = etsim_inputs_core( Param=HParm )
 #'
 etsim_inputs_core <- function( Hazard_type="spline", Param, Tmin=0, Tmax=10, SampleSize=101 ){
 
